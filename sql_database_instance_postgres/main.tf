5783c6bff8cb3ba0caf98744b6cd84effb14a887
Modular Magician <magic-modules@google.com>
2022-03-30 21:50:23 +0000

    New generated code for MM PR 5828.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- sql_database_instance_postgres/main.tf
+++ sql_database_instance_postgres/main.tf
@@ -9,3 +9,11 @@ resource "google_sql_database_instance" "instance" {
   deletion_protection =  "true"
 }
 # [END cloud_sql_postgres_instance_80_db_n1_s2]
+
+# [START cloud_sql_postgres_instance_user]
+resource "google_sql_user" "default" {
+  name     = "users-name"
+  instance = google_sql_database_instance.instance.name
+  password = "changeme"
+}
+# [END cloud_sql_postgres_instance_user]
